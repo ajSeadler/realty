e270e58bb94709b2096597fa1dff0757fe62cb41
ajSeadler <aj6287@gmail.com>
2023-12-23 10:49:57 -0600

    update

--- src/server/db/homesData.js
+++ src/server/db/homesData.js
@@ -1,102 +1,29 @@
-// Sample homes data
+const { faker } = require('@faker-js/faker');
+
+// Function to generate a fake home
+const generateFakeHome = (agentId) => ({
+  address: faker.location.streetAddress(),
+  bedrooms: faker.number.int({ min: 2, max: 5 }),
+  bathrooms: faker.number.int({ min: 1, max: 4 }),
+  square_feet: faker.number.int({ min: 1500, max: 3500 }),
+  price: faker.number.int({ min: 250000, max: 600000 }),
+  year_built: faker.number.int({ min: 1980, max: 2022 }),
+  image_url: `https://picsum.photos/800/600?random=${faker.number.int(1000)}&category=architecture`,
+  zillow_link: faker.internet.url(),
+  agent_id: agentId,
+  bio: faker.lorem.paragraph(),
+});
+
+// Generate 20 fake homes for agent 1
+const fakeHomesAgent1 = Array.from({ length: 20 }, () => generateFakeHome(1));
+
+// Generate 20 fake homes for agent 2
+const fakeHomesAgent2 = Array.from({ length: 20 }, () => generateFakeHome(2));
+
+// Combine fake homes with the original homes
 const homes = [
-  {
-    address: '123 Main St',
-    bedrooms: 3,
-    bathrooms: 2.5,
-    square_feet: 2000,
-    price: 300000,
-    year_built: 1990,
-    image_url: '../images/house2.jpeg',
-    zillow_link: 'https://www.zillow.com/example1',
-    agent_id: 1,
-    bio: 'A cozy family home with three bedrooms and two and a half bathrooms. This charming house is located in a quiet neighborhood, offering a perfect blend of comfort and tranquility.'
-  },
-  {
-    address: '456 Oak Ave',
-    bedrooms: 4,
-    bathrooms: 3,
-    square_feet: 2500,
-    price: 400000,
-    year_built: 2005,
-    image_url: '../images/house10.jpeg',
-    zillow_link: 'https://www.zillow.com/example2',
-    agent_id: 1,
-    bio: 'Spacious and modern, this four-bedroom home is perfect for a growing family. With three bathrooms and ample living space, it provides a comfortable and stylish living environment.'
-  },
-  {
-    address: '734 West Liberty St.',
-    bedrooms: 4,
-    bathrooms: 3.5,
-    square_feet: 2300,
-    price: 330000,
-    year_built: 1997,
-    image_url: '../images/house5.jpeg',
-    zillow_link: 'https://www.zillow.com/example1',
-    agent_id: 2,
-    bio: 'Elegant and sophisticated, this four-bedroom home features three and a half bathrooms and beautiful architectural details. Conveniently located with easy access to amenities.'
-  },
-  {
-    address: '896 Crimson Rd.',
-    bedrooms: 4,
-    bathrooms: 3,
-    square_feet: 2700,
-    price: 430000,
-    year_built: 1919,
-    image_url: '../images/house4.jpeg',
-    zillow_link: 'https://www.zillow.com/example2',
-    agent_id: 1,
-    bio: 'A historic gem! This four-bedroom home, built in 1919, boasts classic charm and character. With three bathrooms and a spacious interior, it offers a unique living experience.'
-  },
-  {
-    address: '144 Second St',
-    bedrooms: 4,
-    bathrooms: 3,
-    square_feet: 3000,
-    price: 420000,
-    year_built: 2009,
-    image_url: '../images/house6.jpeg',
-    zillow_link: 'https://www.zillow.com/example1',
-    agent_id: 2,
-    bio: 'Contemporary luxury at its finest! This four-bedroom home features three bathrooms and a modern design. With a spacious layout and high-end finishes, its a dream home!'
-  },
-  {
-    address: '3390 West Fern Ave',
-    bedrooms: 4,
-    bathrooms: 3,
-    square_feet: 2900,
-    price: 440000,
-    year_built: 2005,
-    image_url: '../images/house7.jpeg',
-    zillow_link: 'https://www.zillow.com/example2',
-    agent_id: 1,
-    bio: "A perfect family retreat! This four-bedroom home offers three bathrooms and a serene atmosphere. With a spacious backyard and modern amenities, it's an ideal living space!"
-  },
-  {
-    address: '998 Coral Ave',
-    bedrooms: 3,
-    bathrooms: 2.5,
-    square_feet: 2700,
-    price: 440000,
-    year_built: 2010,
-    image_url: '../images/house8.jpeg',
-    zillow_link: 'https://www.zillow.com/example1',
-    agent_id: 2,
-    bio: "Charming and inviting! This three-bedroom home features two and a half bathrooms. With a modern kitchen and cozy living spaces, it's a perfect blend of comfort and style."
-  },
-  {
-    address: '648 Cherry Ave',
-    bedrooms: 4,
-    bathrooms: 3,
-    square_feet: 2900,
-    price: 500000,
-    year_built: 2008,
-    image_url: '../images/house9.jpeg',
-    zillow_link: 'https://www.zillow.com/example2',
-    agent_id: 2,
-    bio: "Luxury living awaits! This four-bedroom home boasts three bathrooms and high-end finishes. With a spacious layout and beautiful surroundings, it's an elegant retreat."
-  },
-  // Add more home objects as needed
+  ...fakeHomesAgent1,
+  ...fakeHomesAgent2,
 ];
 
 module.exports = homes;
